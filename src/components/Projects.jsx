7583870727c28982adb28cc0071a7f74a1c0d3e0
tobibamidele <franklintobi09@gmail.com>
2026-03-23 12:23:11 +0100

    Add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
-// Figma asset URLs (valid 7 days from design pull)
-const dartIcon = 'https://www.figma.com/api/mcp/asset/c6076837-f422-408d-869d-26faee2fcb50'
-const rustIcon = 'https://www.figma.com/api/mcp/asset/9d97250a-5f6d-46c2-9847-eb3ea05b8744'
-
 const projects = [
+  {
+    name: 'VaultPapi',
+    description: 'A self-hostable zero-knowledge secure password manager.',
+    githubUrl: 'https://github.com/tobibamidele/vaultpapi',
+    icon: '/icons/go_icon.svg',
+  },
   {
     name: 'Doth',
     description: 'A customizable OAuth 2.0 multi-provider library for dart servers.',
